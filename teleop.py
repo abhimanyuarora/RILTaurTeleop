4577568cfb23e99726ce347825251fe62079a763
Macher <macher@Machers-MacBook-Pro.local>
2020-12-02 12:33:09 +0800

    Added on the spot turning

--- teleop.py
+++ teleop.py
@@ -1,20 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
+import tkinter.font as tkFont
 import serial
 import sys
-from threading import Timer
 import time
 
 try:
     ser = serial.Serial("/dev/tty.usbserial-DN03FUHZ",115200,timeout=1)
+    # ser = serial.Serial("/dev/tty.usbmodem14103",115200,timeout=1)
+    # ser = serial.Serial("/dev/ttys003",115200,timeout=1)
 except:
     print("Couldn't open the serial port")
-    sys.exit(0)
+    # sys.exit(0)
 
 window = tk.Tk()
 # to rename the title of the window
 window.title("Teleop")
-window.minsize(800,500)
+window.minsize(800,300)
+window.geometry("800x600")
 # pack is used to show the object in the window
 
 # You will create two text labels namely 'username' and 'password' and and two input labels for them
@@ -28,25 +31,116 @@ window.rowconfigure(1, weight = 1)
 
 controls_frame = tk.Frame(window,name="controls_frame")
 controls_frame.grid(row = 1, column = 0, sticky = 'NSEW')
-params_frame = tk.Frame(window,name="params_frame")
-params_frame.grid(row = 1, column = 2, sticky = 'NSEW')
+# params_frame = tk.Frame(window,name="params_frame")
+# params_frame.grid(row = 1, column = 2, sticky = 'NSEW')
+
+canvas = tk.Canvas(window,name="canvas")
+scrollbar = tk.Scrollbar(window, orient="vertical", command=canvas.yview, highlightcolor="snow2")
+scrollable_frame = tk.Frame(canvas,width=800)
+params_frame = tk.Frame(canvas,name="params_frame",width=537)
+# scrollbar.pack(side="right", fill="y")
+# params_frame.grid(row = 1, column = 2, sticky = 'NSEW')
+
+# scrollable_frame = tk.Frame(canvas,bg="black")
+
+temp_scroll = scrollbar.get()
+scroll_len = temp_scroll[1]-temp_scroll[0]
+
+canvas_frame = canvas.create_window((0, 0), window=params_frame, anchor="nw")
+
+def configure_frame(e):
+    global params_frame,canvas_frame
+    # print(params_frame['width'])
+    canvas_width = e.width
+    # print("width " + str(canvas_width))
+    # params_frame.config(width=canvas_width)
+    canvas.itemconfig(canvas_frame, width = canvas_width)
+
+def configure_canvas(e):
+    global scroll_len
+    canvas.configure(scrollregion=canvas.bbox("all"))
+    temp_scroll = scrollbar.get()
+    scroll_len = temp_scroll[1]-temp_scroll[0]
+    # scrollbar.set(temp_scroll[0],temp_scroll[1])
+    # canvas.yview_scroll(10, "units")
+    # time.sleep(1)
+    # canvas.yview_scroll(-10, "units")
+    # print(prev_scroll)
+
+params_frame.bind("<Configure>", configure_canvas)
+canvas.bind("<Configure>", configure_frame)
+
+canvas.configure(yscrollcommand=scrollbar.set)
+
+def _on_mousewheel(event):
+    global scroll_len
+    curr_scroll = scrollbar.get()
+    if scroll_len >= 1:
+        return
+    canvas.yview_scroll(-1*(event.delta), "units")
+    print(curr_scroll)
+    print(curr_scroll[1]-curr_scroll[0])
+    # print(  (   max(min(1-scroll_len,curr_scroll[0]-event.delta),0) ,   max(min(1,curr_scroll[0]-event.delta),scroll_len) )   )
+    scrollbar.set(max(min(1-scroll_len,curr_scroll[0]-event.delta),0), max(min(1,curr_scroll[0]-event.delta),scroll_len))
+
+canvas.bind_all("<MouseWheel>", _on_mousewheel)
+
+for i in range(50):
+    ttk.Label(scrollable_frame, text="Sample scrolling label "+str(i)).grid(row=i,sticky="EW")
+
+canvas.grid(row = 1, column = 2, sticky = 'NSEW', pady = (0, 20))
+# scrollable_frame.pack(expand = True) 
+scrollbar.grid(row = 1, column = 3, sticky = 'NS', pady = (0, 20))
 
 ttk.Separator(window, orient='vertical').grid(column=1, row=0, rowspan=2, sticky='NS')
 
+w_frame = tk.Frame(controls_frame,name="w_frame")
+w_frame.grid(row = 0, column = 1, sticky = 'S')
+
+a_frame = tk.Frame(controls_frame,name="a_frame")
+a_frame.grid(row = 1, column = 0, sticky = 'E')
+
+s_frame = tk.Frame(controls_frame,name="s_frame")
+s_frame.grid(row = 2, column = 1, sticky = 'N')
+
+d_frame = tk.Frame(controls_frame,name="d_frame")
+d_frame.grid(row = 1, column = 2, sticky = 'W')
+
+q_frame = tk.Frame(controls_frame,name="q_frame",width=50,height=60)
+q_frame.grid(row = 0, column = 0, sticky = 'SE', padx = (0,20), pady = (0,20))
+
+e_frame = tk.Frame(controls_frame,name="e_frame",width=50,height=60)
+e_frame.grid(row = 0, column = 2, sticky = 'SW', padx = (20,0), pady = (0,20))
+
 # 'Entry' class is used to display the input-field for 'username' text label
-btn_w = tk.Button(controls_frame, text = "W", width = 1, height = 2, name = 'btn_w')
-btn_w.grid(row = 0, column = 1, sticky = 'S')
+tk.Label(w_frame,text="\u2191").grid(row = 0, column = 0)
+btn_w = tk.Button(w_frame, text = "W", width = 1, height = 2, name = 'btn_w')
+btn_w.grid(row = 1, column = 0)
 
-btn_a = tk.Button(controls_frame, text = "A", width = 1, height = 2, name = 'btn_a')
-btn_a.grid(row = 1, column = 0, sticky = 'E')
+tk.Label(a_frame,text="\u2190").grid(row = 0, column = 0)
+btn_a = tk.Button(a_frame, text = "A", width = 1, height = 2, name = 'btn_a')
+btn_a.grid(row = 0, column = 1)
 
-btn_s = tk.Button(controls_frame, text = "S", width = 1, height = 2, name = 'btn_s')
-btn_s.grid(row = 2, column = 1, sticky = 'N')
+tk.Label(s_frame,text="\u2193").grid(row = 1, column = 0)
+btn_s = tk.Button(s_frame, text = "S", width = 1, height = 2, name = 'btn_s')
+btn_s.grid(row = 0, column = 0)
 
-btn_d = tk.Button(controls_frame, text = "D", width = 1, height = 2, name = 'btn_d')
-btn_d.grid(row = 1, column = 2, sticky = 'W')
+tk.Label(d_frame,text="\u2192").grid(row = 0, column = 1)
+btn_d = tk.Button(d_frame, text = "D", width = 1, height = 2, name = 'btn_d')
+btn_d.grid(row = 0, column = 0)
 
-movement_buttons = {'w': btn_w, 'a': btn_a, 's': btn_s, 'd': btn_d}
+tk.Label(q_frame,text="\u21b6",font=("Arial Unicode MS", 25)).place(relx=1,rely=1,anchor="se",y=-35,x=-10)
+btn_q = tk.Button(q_frame, text = "Q", width = 1, height = 2, name = 'btn_q')
+btn_q.place(relx=1,rely=1,anchor="se")
+
+tk.Label(e_frame,text="\u21b7",font=("Arial Unicode MS", 25)).place(relx=0,rely=1,anchor="sw",y=-35,x=10)
+btn_e = tk.Button(e_frame, text = "E", width = 1, height = 2, name = 'btn_e')
+btn_e.place(relx=0,rely=1,anchor="sw")
+
+e_frame.rowconfigure(0,weight=1)
+e_frame.rowconfigure(1,weight=0)
+
+movement_buttons = {'w': btn_w, 'a': btn_a, 's': btn_s, 'd': btn_d, 'q': btn_q, 'e': btn_e}
 
 param_types = ["f","l","s","h","u","d","p"]
 
@@ -55,16 +149,16 @@ params = {
         "f": 0.8,
         "l": 0.15,
         "s": 0.06,
-        "h": 0.18,
+        "h": 0.15,
         "u": 0.03,
         "d": 0.03,
-        "p": 0.5
+        "p": 0.15
     },
     "Trot": {
         "f": 0.8,
         "l": 0.15,
         "s": 0.06,
-        "h": 0.18,
+        "h": 0.15,
         "u": 0.03,
         "d": 0.03,
         "p": 0.5
@@ -95,98 +189,142 @@ controls_frame.columnconfigure(2,weight=1)
 controls_frame.rowconfigure(0,weight=1)
 controls_frame.rowconfigure(1,weight=0)
 controls_frame.rowconfigure(2,weight=1)
+# controls_frame.rowconfigure(3,weight=1)
 
 GaitMode = tk.StringVar(window)
 GaitMode.set("Walk")
 
 tk.Label(params_frame, text="Gait").grid(row=0,column=0)
-tk.OptionMenu(params_frame,GaitMode,"Walk","Trot","Pronk","Bound").grid(row=0,column=1,sticky='EW',padx=10,pady=(10,0))
+tk.OptionMenu(params_frame,GaitMode,"Walk","Trot","Pronk","Bound").grid(row=0,column=1,sticky='EW',padx=(0,30),pady=(10,0))
 
 tk.Label(params_frame, text="Frequency").grid(row=1,column=0)
 tk.Scale(params_frame,orient='horizontal',from_=0,to=10,resolution=0.1,	
-tickinterval=1, name="f").grid(row=1,column=1,sticky='EW',padx=10)
+tickinterval=1, name="f").grid(row=1,column=1,sticky='EW',padx=(0,30))
 
 tk.Label(params_frame, text="Stride Length").grid(row=2,column=0)
-tk.Scale(params_frame,orient='horizontal',from_=0,to=0.15,resolution=0.01,	
-tickinterval=0.05, name="l").grid(row=2,column=1,sticky='EW',padx=10)
+tk.Scale(params_frame,orient='horizontal',from_=0,to=0.2,resolution=0.01,	
+tickinterval=0.05, name="l").grid(row=2,column=1,sticky='EW',padx=(0,30))
 
 tk.Label(params_frame, text="Step Difference").grid(row=3,column=0)
 tk.Scale(params_frame,orient='horizontal',from_=0,to=0.3,resolution=0.01,	
-tickinterval=0.02, name="s").grid(row=3,column=1,sticky='EW',padx=10)
+tickinterval=0.1, name="s").grid(row=3,column=1,sticky='EW',padx=(0,30))
 
 tk.Label(params_frame, text="Stance Height").grid(row=4,column=0)
 tk.Scale(params_frame,orient='horizontal',from_=0,to=0.2,resolution=0.01,	
-tickinterval=0.04, name="h").grid(row=4,column=1,sticky='EW',padx=10)
+tickinterval=0.05, name="h").grid(row=4,column=1,sticky='EW',padx=(0,30))
 
 tk.Label(params_frame, text="Up Amplitude").grid(row=5,column=0)
 tk.Scale(params_frame,orient='horizontal',from_=0,to=0.1,resolution=0.01,	
-tickinterval=0.02, name="u").grid(row=5,column=1,sticky='EW',padx=10)
+tickinterval=0.02, name="u").grid(row=5,column=1,sticky='EW',padx=(0,30))
 
 tk.Label(params_frame, text="Down Amplitude").grid(row=6,column=0)
 tk.Scale(params_frame,orient='horizontal',from_=0,to=0.1,resolution=0.01,	
-tickinterval=0.02, name="d").grid(row=6,column=1,sticky='EW',padx=10)
+tickinterval=0.02, name="d").grid(row=6,column=1,sticky='EW',padx=(0,30))
 
 tk.Label(params_frame, text="Flight %").grid(row=7,column=0)
 tk.Scale(params_frame,orient='horizontal',from_=0,to=1,resolution=0.05,	
-tickinterval=0.2, name="p").grid(row=7,column=1,sticky='EW',padx=10)
+tickinterval=0.2, name="p").grid(row=7,column=1,sticky='EW',padx=(0,30))
 
 params_frame.columnconfigure(0,weight=1)
-params_frame.columnconfigure(1,weight=3)
+params_frame.columnconfigure(1,weight=10)
+# params_frame.rowconfigure(0,weight=1)
+# params_frame.rowconfigure(1,weight=1)
+# params_frame.rowconfigure(2,weight=1)
+# params_frame.rowconfigure(3,weight=1)
+# params_frame.rowconfigure(4,weight=1)
+# params_frame.rowconfigure(5,weight=1)
+# params_frame.rowconfigure(6,weight=1)
+# params_frame.rowconfigure(7,weight=1)
 
 def send_all_params():
     for (key,value) in params[GaitMode.get()].items():
-        window.nametowidget("params_frame."+key).set(value)
-        ser.write(str(key + ' ' + GaitMode.get()[0] + ' ' + str(value) + '\r').encode('utf-8'))
+        window.nametowidget("canvas.params_frame."+key).set(value)
+        ser.write(str(key + ' ' + GaitMode.get()[0] + ' ' + str(value) + '\n').encode('utf-8'))
         if GaitMode.get() == "Trot":
-            ser.write(str(key + ' Y ' + str(value) + '\r').encode('utf-8'))
+            ser.write(str(key + ' Y ' + str(value) + '\n').encode('utf-8'))
 
 send_all_params()
 
 # 'Checkbutton' class is for creating a checkbutton which will take a 'columnspan' of width two (covers two columns)
 # tk.Checkbutton(window, text = "Keep Me Logged In").grid(columnspan = 2) 
 
+current_mode = []
+
 def button1_event(event=None,key=None,type_=None):
     if key:
         widget_name = "btn_" + key
-        event_type =  "ButtonPress" if type_ == "KeyPress" else "ButtonRelease"
+        if type_ == "KeyPress":
+            event_type =  "ButtonPress"
+        elif type_ == "KeyRelease":
+            event_type =  "ButtonRelease"
+        elif type_ == "Multiple":
+            event_type =  "ButtonPress"
+            widget_name = key
     else:
         widget_name = str(event.widget).split(".")[-1]
         event_type = str(event.type)
 
     if event_type == "ButtonPress":
         if widget_name == "btn_w":
+            current_mode.append(widget_name)
             print("SEND ", GaitMode.get(), "Forward")
-            ser.write(str(GaitMode.get()[0]+'\r').encode('utf-8'))
+            ser.write(b'r W 1\n')
+            ser.write(str(GaitMode.get()[0]+'\n').encode('utf-8'))
         elif widget_name == "btn_a":
-            print(str('s Y -' + str(params["Trot"]["s"]) + '\r').encode('utf-8'))
-            ser.write(str('s Y -' + str(params["Trot"]["s"]) + '\r').encode('utf-8'))
-            ser.write(b'Y\r')
+            current_mode.append(widget_name)
+            # print(str('s Y -' + str(params["Trot"]["s"]) + '\r').encode('utf-8'))
+            ser.write(str('s Y -' + str(params["Trot"]["s"]) + '\n').encode('utf-8'))
+            ser.write(b'Y\n')
             print("SEND Trot Left")
             # ser.write(b'\r')
         elif widget_name == "btn_s":
+            current_mode.append(widget_name)
             print("SEND ", GaitMode.get(), "Backward")
+            ser.write(b'r W -1\n')
+            ser.write(str(GaitMode.get()[0]+'\n').encode('utf-8'))
         elif widget_name == "btn_d":
-            print(str('s Y ' + str(params["Trot"]["s"]) + '\r').encode('utf-8'))
-            ser.write(str('s Y ' + str(params["Trot"]["s"]) + '\r').encode('utf-8'))
-            ser.write(b'Y\r')
+            current_mode.append(widget_name)
+            # print(str('s Y ' + str(params["Trot"]["s"]) + '\r').encode('utf-8'))
+            ser.write(str('s Y ' + str(params["Trot"]["s"]) + '\n').encode('utf-8'))
+            ser.write(b'Y\n')
             print("SEND Trot Right")
-    elif event_type == "ButtonRelease" and (widget_name == "btn_w" or widget_name == "btn_a" or widget_name == "btn_s" or widget_name == "btn_d"):
-        print("SEND Stop")
-        ser.write(b'S\r')
+        elif widget_name == "btn_q":
+            current_mode.append(widget_name)
+            # print(str('s Y ' + str(params["Trot"]["s"]) + '\r').encode('utf-8'))
+            # ser.write(str('s Y ' + str(params["Trot"]["s"]) + '\n').encode('utf-8'))
+            # ser.write(b'Y\n')
+            ser.write(b't W -1\n')
+            ser.write(b'T\n')
+            print("SEND Rotate Left")
+        elif widget_name == "btn_e":
+            current_mode.append(widget_name)
+            # print(str('s Y ' + str(params["Trot"]["s"]) + '\r').encode('utf-8'))
+            ser.write(b't W 1\n')
+            ser.write(b'T\n')
+            print("SEND Rotate Right")
+    elif event_type == "ButtonRelease" and (widget_name == "btn_w" or widget_name == "btn_a" or widget_name == "btn_s" or widget_name == "btn_d" or widget_name == "btn_q" or widget_name == "btn_e"):
+        # if type_ == "KeyRelease":
+        current_mode.pop(current_mode.index(widget_name))
+        if len(current_mode) > 0:
+            button1_event(key=current_mode[-1],type_="Multiple")
+            current_mode.pop()
+        else:
+            print("SEND Stop")
+            ser.write(b'S\n')
     
     if event_type == "ButtonRelease":
         if widget_name in param_types:
             print("Setting ", widget_name, " to ", event.widget.get())
             params[GaitMode.get()][widget_name] = event.widget.get()
-            ser.write(str(widget_name + ' ' + GaitMode.get()[0] + ' ' + str(event.widget.get()) + '\r').encode('utf-8'))
+            ser.write(str(widget_name + ' ' + GaitMode.get()[0] + ' ' + str(event.widget.get()) + '\n').encode('utf-8'))
             if GaitMode.get() == "Trot":
-                ser.write(str(widget_name + ' Y ' + str(event.widget.get()) + '\r').encode('utf-8'))
+                ser.write(str(widget_name + ' Y ' + str(event.widget.get()) + '\n').encode('utf-8'))
         elif widget_name == "!optionmenu":
             print("Setting ", widget_name, " to ", GaitMode.get())
             print(params[GaitMode.get()])
             send_all_params()
 
-was_pressed = {'w': False, 'a': False, 's': False, 'd': False}
+was_pressed = {'w': False, 'a': False, 's': False, 'd': False, 'q': False, 'e': False}
             
 def key_event(event):
     if event.char in was_pressed:
@@ -225,27 +363,37 @@ window.bind("<ButtonRelease-1>", button1_event)
 window.bind("<KeyPress>", key_event)
 window.bind("<KeyRelease>", key_event)
 
+exitFlag = False
+
 def on_exit():
-    ser.close()
+    global exitFlag
+    exitFlag = True
     print("Closing serial port")
     window.destroy()
+    ser.close()
 
 window.protocol("WM_DELETE_WINDOW",on_exit)
 # window.mainloop()
 
-t = Timer(2, send_all_params)
 inString = ""
 
 while 1:
+    # window.update()
     try:
         window.update()
+        window.update_idletasks()
     except:
         sys.exit(0)
-    # if (ser.in_waiting):
-    #     c = ser.read()
-    #     if (c == '\n'):
-    #         if inString == "!!!":
-    #             t.start()
-    #         inString = ""
-    #     else:
-    #         inString = inString + c
+
+    if (not exitFlag and ser.in_waiting):
+        c = ser.read()
+        # print(inString)
+        # print(chr(c))
+        if (c == b'\n'):
+            if ''.join(inString) == "!!!":
+                print("Connected")
+                time.sleep(2)
+                send_all_params()
+            inString = ""
+        else:
+            inString = inString + c.decode('utf-8')
